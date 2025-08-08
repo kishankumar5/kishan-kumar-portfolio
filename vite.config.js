5704b80db83ef085ad459d7c396a6e7aebf72498
Kishan Kumar <kishan12387@gmail.com>
2025-08-08 15:27:11 +0530

    Update: built project and fixed preview errors

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // ✅ crucial for Render
+    port: 3000       // optional, but good to specify
+  }
 })
